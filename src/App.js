e7fd4b565e5136e833cfcaf96b7217b355c06d73
zegenerative <zegerdevos@gmail.com>
2019-09-16 15:58:30 +0200

    basic react-redux setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>GUESS DOG</h1>
+    <h2>TRAIN YOUR KNOWLEDGE</h2>
+    <h2>TEST YOUR KNOWLEDGE
+      <p>game 1</p>
+      <p>game 2</p>
+      <p>game 3</p>
+    </h2>
     </div>
   );
 }
