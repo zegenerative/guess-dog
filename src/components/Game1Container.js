7c39b0b5c58acc3ebe63408ba916b9b1a03978cf
AllanRaf <rafale.uk@googlemail.com>
2019-09-17 23:14:44 +0200

    Display answers on page in buttons

--- src/components/Game1Container.js
+++ src/components/Game1Container.js
@@ -28,9 +28,43 @@ class Game1Container extends Component {
         })
     }
 
+    createRandomAnswers = () => {
+
+        let dogAnswers = [this.props.breed]
+        let randomAnswer = null
+        let listOfWrongDogs = this.props.dogsList.filter(dog => dog !== this.props.breed)
+        /*const listOfAllDogs = this.props.dogsList
+        console.log("listOfAllDogs is", listOfAllDogs)
+        const listOfAllDogsLessCorrect = listOfAllDogs.filter(Dog => Dog !== this.props.breed)
+        console.log("listOfAllDoglessCorrect is", listOfAllDogsLessCorrect, "list of correct answer is:", this.props.breed)
+        const randomDogAnswer = listOfAllDogsLessCorrect[Math.floor(Math.random()*listOfAllDogsLessCorrect.length)]
+        
+        const listOfAllDogsLessCorrectPlusOne = listOfAllDogsLessCorrect.filter(Dog => Dog !== randomDogAnswer)
+        console.log("listOfAllDogsLessCorrectPlusone is", listOfAllDogsLessCorrectPlusOne)
+        const randomDogAnswer2 = listOfAllDogsLessCorrectPlusOne[Math.floor(Math.random()*listOfAllDogsLessCorrectPlusOne.length)]
+        console.log("randomDogAnswer2 is ", randomDogAnswer2)
+        dogAnswers = dogAnswers.concat(randomDogAnswer, randomDogAnswer2, this.props.breed)*/
+        
+        //for(let i=0; i< 2;i++){
+        while(dogAnswers.length < 3){
+
+            randomAnswer = listOfWrongDogs[Math.floor(Math.random()*listOfWrongDogs.length)]
+            dogAnswers = dogAnswers.concat(randomAnswer)
+            listOfWrongDogs = listOfWrongDogs.filter(dog => dog !== randomAnswer)
+
+        }
+        //}
+        console.log("dogAnswers length is", dogAnswers.length)
+
+        return dogAnswers
+
+    }
+    
+
     render() {
-      console.log(this.props)
-    return <Game1Visualiser imgUrl={this.props.url} breed={this.props.breed}/>
+      console.log("this.props is", this.props.dogsList)
+      //console.log("random dogs is:", this.createRandomAnswers() )
+    return <Game1Visualiser imgUrl={this.props.url} breed={this.props.breed} dogAnswers = {this.createRandomAnswers()}/>
   }
 }
 
