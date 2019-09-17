d7a7ed715d46ae53238e9686052f304463c0c956
tunguyen-95 <thunguyen.tt@outlook.com>
2019-09-17 10:38:51 +0200

    i changed stuff

--- src/components/DogsListContainer.js
+++ src/components/DogsListContainer.js
@@ -5,17 +5,6 @@ import DogsList from './DogsList'
 export default class DogsListContainer extends Component {
   state = { dogBreeds: null }
 
-  componentDidMount() {
-    request
-      .get('https://dog.ceo/api/breeds/list/all')
-      .then(response => {
-        const breeds = Object.keys(response.body.message)
-        console.log("breeds is: ", breeds)
-        this.updateBreeds(breeds)
-      })
-      .catch(console.error)
-  }
-
   updateBreeds(breeds) {
     this.setState({
       dogBreeds: breeds
