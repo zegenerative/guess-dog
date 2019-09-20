ad0135ce03d789073e87c11a0fff9c3c2105061f
tunguyen-95 <thunguyen.tt@outlook.com>
2019-09-20 15:21:28 +0200

    new score format

--- src/components/Main.js
+++ src/components/Main.js
@@ -14,13 +14,14 @@ class Main extends Component {
     const urlDogImage = "https://s.abcnews.com/images/US/160825_vod_orig_historyofdogs_16x9_992.jpg"
     return (
       <div>
-          <h1>WOOF-WOOF </h1>
-          <Link to="/dogs"><img src ={urlDogImage} width = "50%"  alt = "dog"/></Link>
+          <h1>Main Page</h1>
+          <Link to="/dogs"><img src ={urlDogImage} width = "200px"  alt = "dog"/></Link>
+          <p>Click the image to learn about the range of dog breeds</p>
           <div>
               <Link to="/Game1"><button>Game 1</button></Link>
               <Link to="/Game2"><button>Game 2</button></Link>
               <button>Game 3</button>
-              <Link to='/Credits'><footer>credits</footer></Link>
+              <Link to='/Credits'><footer><button>credits</button></footer></Link>
           </div>
       </div>
     )
