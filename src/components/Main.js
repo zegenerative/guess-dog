c01b6a4fd27c219458ac8b19dff128a29cf386b2
zegenerative <zegerdevos@gmail.com>
2019-09-20 15:08:18 +0200

    Finish styling Main and Game 1

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,6 +3,10 @@ import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { getDogs } from '../actions/getDogs'
 
+const buttonStyle1 = "waves-effect waves-light btn-large"
+const buttonStyle2 = "waves-effect waves-dark btn-small"
+const buttonStyle3 = "btn-floating btn-large waves-effect waves-light red"
+
 class Main extends Component {
 
   componentDidMount() {
@@ -12,15 +16,16 @@ class Main extends Component {
   render() {
     const urlDogImage = "https://s.abcnews.com/images/US/160825_vod_orig_historyofdogs_16x9_992.jpg"
     return (
-      <div>
-          <h1>Main Page</h1>
-          <Link to="/dogs"><img src ={urlDogImage} width = "200px"  alt = "dog"/></Link>
+      <div class="card-panel teal lighten-2">
+          <h1>THE INCREDIBLE DOG QUIZ</h1>
+          <Link to="/dogs"><img src ={urlDogImage} width = "50%"  alt = "dog"/></Link>
           <p>Click the image to learn about the range of dog breeds</p>
           <div>
-              <Link to="/Game1"><button>Game 1</button></Link>
-              <Link to="/Game2"><button>Game 2</button></Link>
-              <button>Game 3</button>
-              <Link to='/Credits'><footer><button>credits</button></footer></Link>
+              <Link to="/Game1"><button class={buttonStyle2}>Game 1</button></Link>
+              <Link to="/Game2"><button class={buttonStyle2}>Game 2</button></Link>
+              <button class={buttonStyle2}>Game 3</button>
+              <br />
+              <Link to='/Credits'><footer class={buttonStyle3}>credits</footer></Link>
           </div>
       </div>
     )
---
+++ src/styles.js
@@ -0,0 +1,3 @@
+export buttonStyle1 = "waves-effect waves-light btn-large"
+export buttonStyle2 = "waves-effect waves-dark btn-small"
+export buttonStyle3 = "btn-floating btn-large waves-effect waves-light red"
